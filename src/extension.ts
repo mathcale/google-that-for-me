c1b2a4c6ee6ec8ca21101e7a199779c222de2baf
Matheus Calegaro <matheuscalegaro@outlook.com>
2019-09-04 22:12:17 -0300

    init

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.googleThatForMe', () => {
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
