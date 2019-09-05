5f881edbf590649985d08715509c6c7ce76da7c8
Matheus Calegaro <matheuscalegaro@outlook.com>
2019-09-04 23:09:32 -0300

    feat: implementing main functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,22 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.googleThatForMe', () => {
-		vscode.window.showInformationMessage('Hello World!');
+export function activate(context: vscode.ExtensionContext): void | boolean {
+	const disposable: vscode.Disposable = vscode.commands.registerCommand('extension.googleThatForMe', () => {
+    const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
+
+    if (typeof editor === 'undefined') return false;
+
+    const selectedText: string = editor.document.getText(editor.selection);
+
+    if (selectedText === '') {
+      vscode.window.showWarningMessage('You need to select some text before running this command!');
+      return false;
+    }
+
+    const selectionAsQS: string = selectedText.toLowerCase().trim().replace(/\s/gm, '+');
+    const googleSearchUrl: string = `https://www.google.com/search?q=${selectionAsQS}`;
+
+    vscode.env.openExternal(vscode.Uri.parse(googleSearchUrl));
 	});
 
 	context.subscriptions.push(disposable);
